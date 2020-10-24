95c7f873c3e32c2fe50e8a25d93dc0025a53d4f1
Kamila Nagorska <gdyniakan@op.pl>
2020-10-24 10:11:38 +0200

    hame page is done

--- Laboratorium1/js/scripts.js
+++ Laboratorium1/js/scripts.js
@@ -11,4 +11,15 @@ function slides() {
     x[myId-1].style.display = "block";
     setTimeout(slides, 5000);
 }
-
+var button = document.getElementById("button");
+button.addEventListener("click", function() {
+    var y = document.getElementById("hideImage");
+        if(y.style.display === "none"){
+            y.style.display = "block";
+            button.style.backgroundColor = "#666699";
+        }
+        else{
+            y.style.display = "none";
+            button.style.backgroundColor = "#8585ad";
+        }
+});
