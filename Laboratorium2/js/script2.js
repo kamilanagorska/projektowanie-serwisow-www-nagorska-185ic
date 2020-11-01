0848b70a51f2768179306c68badd9c5d4c158057
Kamila Nagorska <gdyniakan@op.pl>
2020-11-01 19:43:47 +0100

    yyy

--- Laboratorium2/js/script2.js
+++ Laboratorium2/js/script2.js
@@ -1,11 +1,20 @@
 "use strict";
+var sky = document.getElementsByClassName("d-inline-block")[0];
 function clickDay() {
     document.getElementById("myImage").src="images/sun.png";
-    let sky = document.getElementsByClassName("d-inline-block")[0];
     sky.style.backgroundColor="skyblue";
 }
 function clickNight() {
     document.getElementById("myImage").src="images/moon.png";
-    let sky = document.getElementsByClassName("d-inline-block")[0];
     sky.style.backgroundColor="black";
 }
+var textToClick = document.getElementById("doubleClick");
+textToClick.addEventListener("dblclick", function() {
+    if(sky.style.backgroundColor=="black"){
+        document.getElementById("cytat").innerHTML ="Celuj w księżyc, bo nawet jeśli nie trafisz, będziesz między gwiazdami";
+    }
+    else {
+        document.getElementById("cytat").innerHTML="Koniec końcem, zawsze po burzy wychodzi słońce";
+    }
+})
+
